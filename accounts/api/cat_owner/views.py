2771043d13d070eff994afcf76fd77e003963ace
Ed Geronilla <evgeronilla@vendics.com>
2019-06-21 17:39:01 +0800

    update cat viewset and router. App can now list and retreive cats. nested routers

--- accounts/api/cat_owner/views.py
+++ accounts/api/cat_owner/views.py
@@ -1,5 +1,7 @@
+
 from rest_framework import generics, mixins
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from accounts.models import User
 from accounts.api.permissions import IsOwnerOrReadOnly
@@ -25,15 +27,36 @@ class OwnerCatViewSet(viewsets.ViewSet):
     This viewset automatically provides `list` and `detail` actions.
     """
     permission_classes = [IsSameCatBreed]
-    serializer_class = CatOwnerPublicSerializer
+    serializer_class = CatDetailSerializer, CatSerializer
     lookup_field = 'name'
 
-    def list(self, request, owner_username=None):
-        print(request)
+    def list(self, request, username_username=None):
 
         auth_user_breed_list = Cat.objects.values('breed').distinct().filter(owner=self.request.user)
 
-        return Cat.objects.filter(owner__username=owner_username, breed__in=auth_user_breed_list)
+        queryset = Cat.objects.filter(owner__username=username_username, breed__in=auth_user_breed_list)
+
+        serializer = CatSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, name=None, username_username=None):
+        user = User.objects.get(username=username_username)
+
+        queryset = Cat.objects.get(owner=user, name=name)
+
+        serializer = CatDetailSerializer(queryset)
+        return Response(serializer.data)
+
+
+    # def get_object(self, *args, **kwargs):
+    #     kwargs = self.kwargs
+    #     kw_username = kwargs.get('username')
+    #     kw_name = kwargs.get('name')
+    #
+    #     user = User.objects.get(username=kw_username)
+    #
+    #     return Cat.objects.get(owner=user, name=kw_name)
 
     # def get_queryset(self):
     #     kw_username = self.kwargs['username_username']
