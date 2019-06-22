32c210efec86749d80e20f75603c4ad1ff1fa220
Ed Geronilla <evgeronilla@vendics.com>
2019-06-23 01:19:28 +0800

    update urls.py for nested routes/viewsets

--- dev_test/urls.py
+++ dev_test/urls.py
@@ -1,34 +1,10 @@
-"""dev_test URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_nested import routers
-from accounts.api.cat_owner.views import OwnerViewSet, OwnerCatViewSet
+from accounts.api.cat_owner.urls import router
 
-router = routers.SimpleRouter()
-router.register(r'api/owners', OwnerViewSet, base_name='owner')
-
-owner_router = routers.NestedSimpleRouter(router, r'api/owners', lookup='username')
-owner_router.register(r'cats', OwnerCatViewSet, base_name='owner_cat')
-# router.register(r'api/owners/(?P<username>[\w_/-]+)/cats', views.OwnerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.api.urls')),
-    # path('api/owners/', include('accounts.api.cat_owner.urls')),
-    path('', include(router.urls)),
-    path('', include(owner_router.urls)),
+    path('api/', include(router.urls)),
 ]
